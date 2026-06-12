8478b28659fb02f157d3081410cd77557f3442d5
SrizaGoel <srizagoel@gmail.com>
2026-06-12 20:53:57 +0530

    Fixed requirement file

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-DATABASE_URL = "postgresql://postgres:test@localhost:5432/snaprecall"  
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:test@localhost:5432/snaprecall")  
 # here test = password and snaprecall = dbname and user= postgres
 engine = create_engine(DATABASE_URL)
