405517fa3abb188c488f2a7f653a42733545ea13
SrizaGoel <srizagoel@gmail.com>
2026-07-06 16:55:45 +0530

    testing: updated embedding model

--- backend/embedding.py
+++ backend/embedding.py
@@ -1,8 +1,10 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(
-    "BAAI/bge-base-en-v1.5"
-)
+# model = SentenceTransformer(
+#     "BAAI/bge-base-en-v1.5"
+# )
+
+model = SentenceTransformer("BAAI/bge-small-en-v1.5")
 
 def generate_embedding(text):
     return model.encode(text).tolist()
